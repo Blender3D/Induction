7ff5339e34a30855fb9c9f2c89ffca0973c4b615
Blender3D <452469+Blender3D@users.noreply.github.com>
2011-04-10 14:06:56 -0400

    PATHTRACING WORKS!!!

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'gui.ui'
 #
-# Created: Mon Apr  4 21:21:40 2011
+# Created: Sun Apr 10 14:06:47 2011
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
